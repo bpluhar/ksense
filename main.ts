4e787829ae342f484343d1387a7e508883f70de0
Brian Pluhar <pluharbrian@gmail.com>
2025-10-14 19:18:13 -0400

    storing data in variables with types

--- main.ts
+++ main.ts
@@ -3,7 +3,22 @@ import { Patient, Pagination, Metadata } from './types';
 const API_KEY = 'ak_322e3646b8b8083e7cbd7890a9ca2fc73b08552d5093de17';
 const BASE_URL = 'https://assessment.ksensetech.com/api';
 
-async function fetchPatientData() {
+let patients: Patient[] = [];
+let pagination: Pagination = {
+  page: 1,
+  limit: 10,
+  total: 0,
+  totalPages: 0,
+  hasNext: false,
+  hasPrevious: false
+};
+let metadata: Metadata = {
+  timestamp: '',
+  version: '',
+  requestId: ''
+};
+
+async function fetchPatientData(): Promise<void> {
   const response = await fetch(`${BASE_URL}/patients`, {
     method: 'GET',
     headers: {
@@ -11,9 +26,13 @@ async function fetchPatientData() {
     }
   });
   const data = await response.json();
-  return data;
+  patients = data.data;
+  pagination = data.pagination;
+  metadata = data.metadata;
 }
 
-fetchPatientData().then(data => {
-  console.log(data);
+
+
+fetchPatientData().then(() => {
+  console.log(patients, pagination, metadata);
 });
