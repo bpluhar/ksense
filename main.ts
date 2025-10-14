e68d2862da6a6036aa97d760c896ca4a6e32d764
Brian Pluhar <pluharbrian@gmail.com>
2025-10-14 19:09:08 -0400

    Added tsx for ts running, basic API call working.

---
+++ main.ts
@@ -0,0 +1,17 @@
+const API_KEY = 'ak_322e3646b8b8083e7cbd7890a9ca2fc73b08552d5093de17';
+const BASE_URL = 'https://assessment.ksensetech.com/api';
+
+async function fetchPatientData() {
+  const response = await fetch(`${BASE_URL}/patients`, {
+    method: 'GET',
+    headers: {
+      'x-api-key': API_KEY
+    }
+  });
+  const data = await response.json();
+  return data;
+}
+
+fetchPatientData().then(data => {
+  console.log(data);
+});
